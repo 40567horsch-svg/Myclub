913d85a1f4876cc110261e805a2beba7962ddd2f
40567horsch-svg <40567horsch@gmail.com>
2025-10-18 11:44:14 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function student_status() {
+    var stu_stat = prompt('Are you a student?){
+    if (stu_stat == 'yes' || 'no'){
+        document.alert("Thank you for your response.")
+    }else{
+        alert("please enter yes or no.") 
+}
